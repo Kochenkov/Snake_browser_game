5347e77214a32c73ceb199f16b779908ed50ecef
Kochenkov Vladislav <rddr@mail.ru>
2018-11-07 15:44:03 +0300

    add a random function

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ var ctx = cvs.getContext("2d");
 var wid = canv1.width;
 var hei = canv1.height;
 var blockSize = 20;
-var row = 2;
-var col = 4;
+var rowMax = hei/blockSize; 
+var colMax = wid/blockSize;
+var row = 4;
+var col = 6;
+var rowRandom;
+var colRandom;
 var click;
+
+//функция генерации случайной строки и столбца для кубика-еды:
+var random = function(){
+	rowRandom = Math.round(Math.random()*rowMax);
+	colRandom = Math.round(Math.random()*colMax);
+}
+
 
 //установка интервала для перемещения:
 var myTimer = setInterval(move, 500);
@@ -21,6 +32,9 @@ var Block = function(col,row) {
 		ctx.fillRect(x, y, blockSize, blockSize);
 	}
 }
+
+
+
 //наполняем змейку сегментами:
 var segments = [
 		new Block(col-4,row),
@@ -36,8 +50,10 @@ var draw = function() {
 	ctx.fillRect(0, 0, wid, hei);
 	for(var i=0; i<segments.length; i++) {
 		segments[i].drawSquare('red');
-		}
 	}
+}
+	
+	
 //функция изменения позиции змейки:
 var changeSnake = function() {
 	segments.push(new Block(col,row));
@@ -49,16 +65,16 @@ var changeSnake = function() {
 document.onkeydown = function(e) {
 	if ((e.keyCode==68)&&(click!='s')) {
 		click = 'w';
-		}
+	}
 	if ((e.keyCode==65)&&(click!='w')) {
 		click = 's';
-		}
+	}
 	if ((e.keyCode==87)&&(click!='d')) {
 		click = 'a';
-		}
+	}
 	if ((e.keyCode==83)&&(click!='a')) {
 		click = 'd';
-		}
+	}
 }
 //события изменения координат блоков по нажатию на кнопки:
 function move() {
@@ -79,5 +95,10 @@ function move() {
 		changeSnake();
 	}
 }
-//рисуем змейку в начале программы:
+//начало программы:
 draw();
+// нужно будет разобраться и переделать!!!
+random();
+var eat = new Block(colRandom,rowRandom);
+eat.drawSquare("blue");
+
